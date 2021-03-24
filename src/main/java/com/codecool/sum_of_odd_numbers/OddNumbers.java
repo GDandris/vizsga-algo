b72e3bcd2fc88013eb6d54bbaf8ee210e5bec32f
GDandris <andris.gede@gmail.com>
2021-03-24 15:12:32 +0100

    tasks done

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -1,10 +1,15 @@
 package com.codecool.sum_of_odd_numbers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        return numbers
+                .stream()
+                .filter(i -> i % 2 != 0)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
